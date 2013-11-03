a3bf231ab24f30f6162a415911857cddc6603eef
Andrew Stocker <sleepyams@gmail.com>
2013-11-03 16:11:43 -0500

    new script which grabs data w/o test visualization and outputs to csv

---
+++ v7/reader.py
@@ -0,0 +1,184 @@
+import praw
+import csv
+from math import sqrt
+import random
+import time
+import os
+from sys import argv
+
+cx = 1024/2
+cy = 768/2
+inc = 4
+INTERVALS = argv[1]
+
+### -----DATA-----
+
+tracked_threads = {}
+tracked_threads_queue = []
+tracked_users = {}
+tracked_users_colors = {}
+
+### -----FUNCTIONS-----
+
+def increment(v1, v2, d):
+    p = (v2[0]-v1[0], v2[1]-v1[1])
+    if p == (0, 0):
+        return v1
+    s = d/sqrt(p[0]**2 + p[1]**2)
+    p = (p[0]*s, p[1]*s)
+    return (v1[0]+p[0], v1[1]+p[1])
+
+def init_parent(thread, alist, user_dict, color_dict):
+    author = str(thread.author)
+    user_dict[author]= (
+        random.uniform(-1*cx, cx),
+        random.uniform(-1*cy, cy)
+        )
+    color = (
+        random.randint(0,255),
+        random.randint(0,255),
+        random.randint(0,255)
+        )
+    color_dict[author] = color
+    # csv
+    alist.append('new')
+    alist.append(author)
+    alist.append(color)
+
+def update_users(parent, comments, alist, user_dict, comment_list, color_dict):
+    for comment in comments:
+        if comment.__class__.__name__ != 'MoreComments':
+            if comment.fullname not in comment_list:
+                comment_list.append(comment.fullname)
+                author = str(comment.author)
+                if author not in user_dict:
+                    user_dict[author] = (
+                        random.uniform(-1*cx, cx),
+                        random.uniform(-1*cy, cy)
+                        )
+                    color = (
+                        random.randint(0,255),
+                        random.randint(0,255),
+                        random.randint(0,255)
+                        )
+                    color_dict[author] = color
+                    # csv
+                    alist.append('new')
+                    alist.append(author)
+                    alist.append(color)
+                else:
+                    try:
+                        npos = increment(
+                            user_dict[author],
+                            user_dict[str(parent.author)],
+                            inc
+                            )
+                        user_dict[author] = npos
+                        # csv
+                        alist.append(author)
+                        alist.append(npos)
+                        
+                    except KeyError as e:
+                        args = e.args
+                        print "===== USERNAME KEY ERROR"
+                        print "=====", args[0], "user could not be found"
+            if len(comment.replies) > 0:
+                update_users(
+                    comment,
+                    comment.replies,
+                    alist,
+                    user_dict,
+                    comment_list,
+                    color_dict)
+
+def update_threads(reddit, threads_dict, threads_queue, lim):
+    top = reddit.get_front_page(limit=lim)
+    for thread in top:
+        if thread.id not in threads_queue:
+            threads_queue.insert(0, thread.id)
+            threads_dict[thread.id] = []
+        else:
+            del threads_queue[threads_queue.index(thread.id)]
+            threads_queue.insert(0, thread.id)
+
+
+
+### -----MAIN-----
+            
+r = praw.Reddit('Reddit graphing script -- amstocker.wordpress.com')
+THREAD_COUNT = 50
+f = open("actions.csv", "w")
+w = csv.writer(f)
+
+CONT = True
+while CONT == True:
+    i = int(INTERVALS)
+    while i > 0:
+        actionlist = []
+        connected = False
+        while connected == False:
+            try:
+                update_threads(
+                    r,
+                    tracked_threads,
+                    tracked_threads_queue,
+                    THREAD_COUNT
+                    )
+                connected = True
+            except:
+                print "===== CONNECTION ERROR"
+                time.sleep(30)
+        for thread_id in tracked_threads_queue[:THREAD_COUNT]:
+            connected = False
+            while connected == False:
+                try:
+                    thread = r.get_submission(submission_id = thread_id)
+                    init_parent(
+                        thread,
+                        actionlist,
+                        tracked_users,
+                        tracked_users_colors
+                        )
+                    update_users(
+                        thread,
+                        thread.comments,
+                        actionlist,
+                        tracked_users,
+                        tracked_threads[thread_id],
+                        tracked_users_colors
+                        )
+                    connected = True
+                except:
+                    print "===== CONNECTION ERROR"
+                    time.sleep(30)
+                
+            print "updated @",time.time(),"//",thread_id,"//",len(tracked_threads[thread_id]),"comments"
+        w.writerow(actionlist)
+        i -= 1
+        print "updated @",time.time(),"//",len(tracked_users),"users // approx.",i*0.035,"hours left" 
+
+    valid = False
+    while valid == False:
+        try:
+            n = int(raw_input("moar data? (0 to end) > "))
+            if n == 0:
+                CONT = False
+                valid = True
+            else:
+                INTERVALS = n
+                CONT = True
+                valid = True
+        except:
+            print 'invalid entry'
+
+f.close()
+        
+
+
+
+
+
+
+
+    
+
---
+++ v7/visual.py
@@ -0,0 +1,61 @@
+import pygame
+from pygame.locals import *
+import os
+import csv
+from sys import argv
+
+# pygame
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+w, h = 1024, 768
+bg_color = (255,236,217)
+cx, cy = w/2, h/2
+
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((w, h))
+screen.fill(bg_color)
+pygame.display.flip()
+
+eraser = pygame.Surface((w, h))
+eraser.fill(bg_color)
+eraser.set_alpha(10)
+
+tracking = {}
+
+def render_user(p, color):
+    pygame.draw.circle(screen, (0,0,0), p, 5)
+    pygame.draw.circle(screen, color, p, 4)
+
+f = open(argv[1], "r")
+reader = csv.reader(f)
+for actions in reader:
+    screen.blit(eraser, (0,0))
+    index = 0
+    length = len(actions)
+    while index < length:
+        action = actions[index]
+        if action == 'new':
+            exec('tracking[actions[index+1]] = ' + actions[index+2])
+            index += 3
+        else:
+            exec('pos = ' + actions[index+1])
+            dest = (int(cx+pos[0]), int(cy+pos[1]))
+            render_user(dest, tracking[action])
+            index += 2
+    pygame.display.flip()
+
+f.close()
+
+done = False
+while done == False:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            done = True
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                done = True
+
+pygame.quit()
+
+
+    
